a3da0de8791eeaaf58eaec49543c2063514b0be6
Amaia <asuarezs@centro.edu.mx>
2025-12-03 10:54:22 -0600

    pqnsv

--- js/testimonios.js
+++ js/testimonios.js
@@ -2,4 +2,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
     },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
 });
