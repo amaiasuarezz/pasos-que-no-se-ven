819013742ebf43299efd8996ce2a1c2f3954b4fd
JuanFuent.es <input@juanfuent.es>
2025-12-05 11:49:55 -0600

    Add Preloader

--- js/preloader.js
+++ js/preloader.js
@@ -2,20 +2,27 @@
 const preloaderContainer = document.getElementById('preloader');
 const enterButton = document.getElementById('enter-button');
 
-const timeline = gsap.timeline({
-    paused: true,
-}).to(enterButton, {
+// Timeline de animaciones
+const timeline = gsap.timeline({ paused: true }) // Pausada por defecto
+// Animación del botón
+timeline.to("#enter-button", {
     duration: 2,
     width: '200%',
-    ease: 'power2.inOut',
-    onStart: () => {
-        enterButton.classList.remove('loading');
-    },
-    onComplete: () => {
-    },
-    onReverseComplete: () => {
-        enterButton.classList.add('loading');
-    }
-}, 0);
+    ease: 'power2.inOut'
+}, 0)
 
+// Animación del header
+timeline.to("#main-header", {
+    marginTop: 0,
+    duration: 1, // Duración de animación
+    ease: 'power2.inOut',
+}, .5); // Delay de la animación
+
+/* Evento de clic en el botón
+* Inicia la animación de la timeline
+*/
 enterButton.addEventListener('click', () => timeline.play());
+
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
+});
